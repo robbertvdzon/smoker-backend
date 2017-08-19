152a87a25fffc5ef1467cecfc658a454016a54fa
robbertvdzon <robbert@vdzon.com>
2017-08-19 14:19:17 +0200

    update

--- src/main/kotlin/com/vdzon/smoker/ApplicationRoutes.kt
+++ src/main/kotlin/com/vdzon/smoker/ApplicationRoutes.kt
@@ -15,11 +15,11 @@ object ApplicationRoutes {
 
 
     fun routes(smokerlogRepository: SmokerlogRepository?): RouterFunction<*> {
-        return (route(GET("/getfirst"),
+        return (route(GET("/getlast"),
                 HandlerFunction { r -> getMono(smokerlogRepository) }
         ))
                 .and(route(GET("/getall"),
-                        HandlerFunction { r -> getFlux(smokerlogRepository) }
+                        HandlerFunction { r -> getFlux(r, smokerlogRepository) }
                 ))
                 .and(route(GET("/add"),
                         HandlerFunction { r -> add(r, smokerlogRepository) }
@@ -28,32 +28,33 @@ object ApplicationRoutes {
     }
 
     private fun add(r: ServerRequest?, smokerlogRepository: SmokerlogRepository?): Mono<ServerResponse> {
-        println("Adding!")
         val tempString = r!!.queryParam("temp");
-        println("Adding 1")
         val sturingString = r!!.queryParam("sturing");
-        println("Adding 2")
         val temp = tempString.map { Integer.valueOf(it) }.orElse(0)
-        println("Adding 3")
         val sturing = sturingString.map { Integer.valueOf(it) }.orElse(0)
-        println("Adding 4")
         val domain: SmokerLog = SmokerLog(UUID.randomUUID(), Date(), temp, sturing)
-        println("Adding 5")
         smokerlogRepository!!.save(domain).block()
-        println("Adding 6")
         val body: Mono<ServerResponse> = ServerResponse.ok().body(Mono.just("ok"), String::class.java)
         return body
     }
 
-    private fun getFlux(smokerlogRepository: SmokerlogRepository?): Mono<ServerResponse> {
-        val findAll: Flux<SmokerLog> = smokerlogRepository!!.findAll()
+    private fun getFlux(r: ServerRequest?, smokerlogRepository: SmokerlogRepository?): Mono<ServerResponse> {
+        val range = r!!.queryParam("range");
+        val findAll: Flux<SmokerLog> =
+                if (range.isPresent) {
+                    println(range.get())
+                    smokerlogRepository!!.findAll()
+                } else {
+                    smokerlogRepository!!.findAll()
+                };
         val body = ServerResponse.ok().body(findAll, SmokerLog::class.java)
         return body
     }
 
 
     private fun getMono(smokerlogRepository: SmokerlogRepository?): Mono<ServerResponse> {
-        val body: Mono<ServerResponse> = ServerResponse.ok().body(getFirstByDomain(smokerlogRepository), SmokerLog::class.java)
+//        val body: Mono<ServerResponse> = ServerResponse.ok().body(getFirstByDomain(smokerlogRepository), SmokerLog::class.java)
+        val body: Mono<ServerResponse> = ServerResponse.ok().body(Mono.just(SmokerLog(UUID.randomUUID(),Date(),12,13)), SmokerLog::class.java)
         return body
     }
 
--- src/main/kotlin/com/vdzon/smoker/SmokerlogRepository.kt
+++ src/main/kotlin/com/vdzon/smoker/SmokerlogRepository.kt
@@ -1,11 +1,11 @@
 package com.vdzon.smoker
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface SmokerlogRepository : ReactiveCrudRepository<SmokerLog, Long> {
 
     fun findFirstByDate(): Mono<SmokerLog>
-
 
 }
