701c8ca88c40488b3ff89e85b258ae9e0e5dc055
robbe <robbert@vdzon.com>
2017-09-10 21:18:03 +0200

    reload after change range

--- angular/src/app/grafiek/grafiek.component.ts
+++ angular/src/app/grafiek/grafiek.component.ts
@@ -9,18 +9,8 @@ import {SmokerserviceService} from "../smokerservice.service";
 })
 export class GrafiekComponent implements OnInit {
   userId: String;
-  pieChartData =  {
-    chartType: 'LineChart',
-    dataTable: [],
-    options:
-      {
-        title: 'Smoker',
-        curveType: 'function',
-        hAxis: {title: 'Tijd',
-        slantedText:true, slantedTextAngle:80},
-        vAxis: {minValue: 80},
-      }
-  };
+  dataTable: any[] = [];
+  pieChartData =  {};
 
   constructor(private route: ActivatedRoute, private smokerserviceService:SmokerserviceService) { }
 
@@ -34,16 +24,33 @@ export class GrafiekComponent implements OnInit {
   private loadGrafiek(range:String) {
     this.smokerserviceService.getAll(this.userId, range).subscribe(data => {
       let smokerData:any[] = <Array<any>>data;
-      this.pieChartData.dataTable=[];
-      this.pieChartData.dataTable.push(['Tijd', 'Temp', 'Fan'])
-      for (var i=0; i<smokerData.length; i++){
-        this.pieChartData.dataTable.push([new Date(smokerData[i].date), smokerData[i].temp, smokerData[i].sturing]);
+
+
+      if (this.dataTable.length==0) {
+        this.dataTable.push(['Tijd', 'Temp', 'Fan']);
       }
+      for (var i=0; i<smokerData.length; i++){
+        this.dataTable.push([new Date(smokerData[i].date), smokerData[i].temp, smokerData[i].sturing]);
+      }
+
+      this.pieChartData =  {
+        chartType: 'LineChart',
+        dataTable: this.dataTable,
+        options:
+          {
+            title: 'Smoker',
+            curveType: 'function',
+            hAxis: {title: 'Tijd',
+              slantedText:true, slantedTextAngle:80},
+            vAxis: {minValue: 80},
+          }
+      };
+
+
     });
   }
 
   setPlan(value) {
-    //if you're on older versions of ES, use for-in instead
     this.loadGrafiek(value);
   }
 }
