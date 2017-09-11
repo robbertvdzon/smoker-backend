038585ffd7c6d6c8e07fab8cb2f30a130484a30f
robbe <robbert@vdzon.com>
2017-09-11 21:06:13 +0200

    test met grafiek

--- angular/src/app/grafiek/grafiek.component.ts
+++ angular/src/app/grafiek/grafiek.component.ts
@@ -11,18 +11,7 @@ export class GrafiekComponent implements OnInit {
   userId: String;
   smokerData:any;
   dataTable: any[] = [];
-  pieChartData =  {
-    chartType: 'PieChart',
-    dataTable: [
-      ['Task', 'Hours per Day'],
-      ['Work',     11],
-      ['Eat',      2],
-      ['Commute',  2],
-      ['Watch TV', 2],
-      ['Sleep',    7]
-    ],
-    options: {'title': 'Tasks'},
-  };
+
   chartData =  {
     chartType: 'LineChart',
     dataTable: [],
